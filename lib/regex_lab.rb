06f058e6e5d68de9be68a00e66487e053d9c51b5
Jeffrey Richman <jrichman2009@gmail.com>
2018-08-25 18:40:41 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -2,6 +2,7 @@ def starts_with_a_vowel?(word)
 match = ["afoot Excellent incredible Absolute unreal Inconceivable octopus"]
 match.each do |word|
   if word.scan(/^[aeiou]/)
+    return true
   end
 end
 
